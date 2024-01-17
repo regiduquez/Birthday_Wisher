2eaf939690f6c9d639092002c4a461ba004804f8
regiduquez <regiduquez@gmail.com>
2024-01-17 15:03:52 +0800

    Automated Quotes Mailing Program Initial Stage

--- main.py
+++ main.py
@@ -4,34 +4,26 @@ import datetime as dt
 import random as rd
 
 DAYS_OF_WEEK = ["Monday","Tuesday","Wednesday","Thursday", "Friday", "Saturday", "Sunday"]
-
-with open("quotes.txt", 'r') as quotes:
-    quotes_list = quotes.readlines()
-quotes_day = rd.choice(quotes_list)
-print(quotes_day)
-
 now = dt.datetime.now()
-this_day = now.weekday()
 
+with open("quotes.txt") as quotes:
+    quotes_list = quotes.readlines()
+    quotes_day = rd.choice(quotes_list)
 
 def send_mail():
     my_email = "reginaldduquez@outlook.com"
     my_password = "#AbbyRegi4ever"
     recipient_email = "reginaldduquez@yahoo.com"
 
-    subject = f"Happy {DAYS_OF_WEEK[this_day]} this is your quote of the day!"
+    subject = f"Happy {DAYS_OF_WEEK[now.weekday()]} this is your quote of the day!"
     body = quotes_day
-
-    em = EmailMessage()
-    em['From'] = my_email
-    em['To'] = recipient_email
-    em['Subject'] = subject
-    em.set_content(body)
 
     with smtplib.SMTP("smtp-mail.outlook.com", port=587) as smtp:
         smtp.starttls()
         smtp.login(my_email, my_password)
-        smtp.sendmail(my_email, recipient_email, em.as_string())
+        smtp.sendmail(my_email, recipient_email, msg=f"Subject:{subject}\n\n{body}")
+
+
 
 if now.weekday() == DAYS_OF_WEEK.index("Wednesday"):
     send_mail()

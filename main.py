b2ed865ab9d774b1a43e1b93f0596f4a8161458d
regiduquez <regiduquez@gmail.com>
2024-01-17 22:06:01 +0800

    Birthday Greeting Auto Emailer Initial(functional)

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import datetime as dt
 import random as rd
 import smtplib
 
-MY_EMAIL = 'reginaldduquez@outlook.com'
-MY_PASS = '#AbbyRegi4ever'
+MY_EMAIL = 'reginaldduquez@gmail.com'
+MY_PASS = 'wzceszqyiwhngdrz'
 
 
 
@@ -26,7 +26,7 @@ for i in bday_list:
             letter_to_send = let_temp.read()
             letter_to_send = letter_to_send.replace('[NAME]', i['name'])
 # 4. Send the letter generated in step 3 to that person's email address.
-        with smtplib.SMTP('smtp-mail.outlook.com', port=587) as smtp:
+        with smtplib.SMTP('smtp.gmail.com', port=587) as smtp:
             smtp.starttls()
             smtp.login(MY_EMAIL, MY_PASS)
             smtp.sendmail(MY_EMAIL,i['email'], msg=f"Subject:Happy Birthday!\n\n{letter_to_send}")
